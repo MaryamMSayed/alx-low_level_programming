3a9da73d0ca3e779b43cfe5cc99e3e2eb72bc93a
MaryamMSayed <mariam.s.moneem@gmail.com>
2023-04-20 22:36:48 +0200

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - A main function to printf
+*Return: Always return 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
