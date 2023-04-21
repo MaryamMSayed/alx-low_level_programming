63e386a240adfc004abab31669832053eaaa6a02
MaryamMSayed <mariam.s.moneem@gmail.com>
2023-04-21 22:04:35 +0200

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: [0] in case success
+*/
+int main(void)
+{
+char c;
+for (c = 'z' ; c >= 'a' ; --c)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
